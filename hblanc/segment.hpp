a7f45ae34179d27087f92fdab33fbc9d6141eb86
hugo blanc <hblanc@gaming.tech>
2022-02-09 14:36:53 +0100

    tout bien a sa place dans le front de ton daron

--- hblanc/segment.hpp
+++ hblanc/segment.hpp
@@ -1,6 +1,21 @@
 #pragma once
+#define MOVECASE 10
 
-class Segment {
+class Segment
+{
+public:
+  Segment(int x, int y);
+  ~Segment();
+
+  int GetX();
+  int GetY();
+  void SetX(int);
+  void SetY(int);
+
+  Segment *GetNext();
+  void SetNext(Segment *next);
+
 private:
-  Segment* next;
+  int x, y;
+  Segment *next;
 };
